6f462606f4898acd59771fa23f4af1571e61e4fc
Ridzwan <4947140+omgsian@users.noreply.github.com>
2023-09-27 16:45:42 +0800

    added 3 subnets: 1 private, 2 public

--- multi-server-lb-deploy/main.tf
+++ multi-server-lb-deploy/main.tf
@@ -1,10 +1,31 @@
 data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
+  cidr_block = "10.0.0.0/23" # 512 IPs 
+  tags = {
+    Name = "prod_vpc"
   }
 }
+
+# Public subnet
+data "aws_subnet" "prod_subnet1" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.0.0/27" #32 IPs
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+}
+
+# Public subnet
+data "aws_subnet" "prod_subnet2" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.0.32/27" #32 IPs
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+}
+
+# Private subnet 
+data "aws_subnet" "prod_subnet2" {
+  vpc_id                  = aws_vpc.default.id
+  cidr_block              = "10.0.1.0/27" #32 IPs
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+}
+
