5e9e01011d24664968bc835b50a313e3ec297f86
Ridzwan <4947140+omgsian@users.noreply.github.com>
2023-09-27 18:13:06 +0800

    changed lb target group name

--- multi-server-lb-deploy/main.tf
+++ multi-server-lb-deploy/main.tf
@@ -112,7 +112,7 @@ resource "aws_lb" "prod_lb" {
 }
 
 resource "aws_lb_target_group" "prod_lb_tg" {
-  name     = "sh-tf-lb-alb-tg"
+  name     = "load balancer target group"
   port     = var.server_ports[1].port
   protocol = "HTTP"
   vpc_id   = aws_vpc.default.id
